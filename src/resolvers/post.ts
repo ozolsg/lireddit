a4ae027e98be3553831dd460f8de94350ba29336
Girts Ozols <girtsozols@gmail.com>
2020-12-23 18:48:08 +0200

    validating specific queries

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -16,6 +16,7 @@ import {
 import { MyContext } from "../types";
 import { isAuth } from "../middleware/isAuth";
 import { getConnection } from "typeorm";
+import { Updoot } from "../entities/Updoot";
 
 @InputType()
 class PostInput {
@@ -38,6 +39,39 @@ export class PostResolver {
   @FieldResolver(() => String)
   textSnippet(@Root() root: Post) {
     return root.text.slice(0, 150);
+  }
+
+  @Mutation(() => Boolean)
+  @UseMiddleware(isAuth)
+  async vote(
+    @Arg("postId", () => Int) postId: number,
+    @Arg("value", () => Int) value: number,
+    @Ctx() { req }: MyContext
+  ) {
+    const isUpdoot = value !== -1;
+    const realValue = isUpdoot ? 1 : -1;
+    const { userId } = req.session;
+    // await Updoot.insert({
+    //   userId,
+    //   postId,
+    //   value: realValue,
+    // });
+    console.log("Updoot", Updoot);
+    getConnection().query(
+      `
+    START TRANSACTION;
+
+    insert into updoot ("userId", "postId", value)
+    values(${userId},${postId},${realValue});
+
+    update post
+    set points = points + ${realValue}
+    where id = ${postId};
+
+    COMMIT;
+    `
+    );
+    return true;
   }
 
   @Query(() => PaginatedPosts)
@@ -84,8 +118,6 @@ export class PostResolver {
     //   }); // cursor gives us the position and from that position we say how many do we want to show with limit
     // }
     // const posts = await qb.getMany();
-
-    console.log("posts:", posts);
 
     return {
       posts: posts.slice(0, realLimit),
